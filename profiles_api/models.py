89cd1d07300d7fdc8a61c71a90a4dcfcde229d48
Dipesh <dipeshghag1234@gmail.com>
2024-01-30 13:13:57 +0530

    Added custom user profile model, manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email address')
+
+        # Normalize email address
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+
+        # Set password
+        user.set_password(password)
+
+        # Save user model
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, name, password):
+        """Create and save new superuser with given details"""
+        user = self.create_user(email, name, password)
+
+        # Set superuser status to True
+        user.is_superuser = True
+        user.is_staff = True
+
+        # Save user model
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique = True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve short name of user"""
+        return self.name
+
+    # Recommended to add this to our custom user model
+    # to ensure Django knows how to convert our user object to a string
+    def __str__(self):
+        """Return string representation of our user"""
+        return self.email
