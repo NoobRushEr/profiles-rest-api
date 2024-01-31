4c7422adb22449a7c8cf3fe87e5b78a9af4a5650
Dipesh <dipeshghag1234@gmail.com>
2024-01-31 11:00:28 +0530

    Added HelloApiView

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # Add include to the import statement
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('profiles_api.urls')) # Add this line to the urlpatterns list (include() function
 ]
